92920e55c0e9d92ae09f4d452a69873c7e264f5e
Simon Wallmann <s.wallmann@kompsoft.de>
2015-01-08 19:51:23 +0100

    Bump version to 1.0.0

--- eden.class.php
+++ eden.class.php
@@ -26,7 +26,7 @@ if ( !defined('EQDKP_INC') ){
 if(!class_exists('eden')) {
 	class eden extends game_generic {
 		protected static $apiLevel	= 20;
-		public $version				= '0.1';
+		public $version				= '1.0.0';
 		protected $this_game		= 'eden';
 		protected $types			= array('classes', 'races', 'filters');
 		protected $classes			= array();
